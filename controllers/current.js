b27e389453b08c60018d5eadabe8c9d3eabfc9cb
oluwatosin <gnztrades@gmail.com>
2022-01-27 23:31:23 +0100

    current controller update

--- controllers/current.js
+++ controllers/current.js
@@ -5,7 +5,9 @@ const current = {};
 // Controller first checks to see if the particular row exists or not, this determines
 // If the row will be created or displayed.
 current.get = (req, res, next) => {
+    // use current_id, equipment_name and level to recognize a current item
     const { query } = req;
+    // Add date query to this in order to select other days
     db.query(model.get, [query["current_id"]])
         .then(resp => {
             res.send({res: resp.rows[0] })
@@ -13,18 +15,19 @@ current.get = (req, res, next) => {
 }
 
 current.post = (req, res, next) => {
+    // In order to recognize other category of current, add equipment_name, level and type to what to extract from query and add to database
     const { data } = req.body;
     const { query } = req;
+    const { current_id,  station, feeder_name, type, level } = query;
     // check if the data exists then switch between posting and updating    
-    db.query(model.get, [query["current_id"]])
+    db.query(model.get, [current_id])
         .then(resp => {            
             if(resp.rowCount > 0) {
-                db.query(model.update, [data, query["current_id"]]);
+                db.query(model.update, [data, current_id]);
             } else {
                 const hour = query["current_id"].split('-').pop();
-                const feeder_name = query["feeder_name"];
                 const equipment_id = 3;                
-                db.query(model.create, [Date().split(' ').slice(0, 4).join(' '), hour, data, equipment_id, feeder_name, query["current_id"]]);
+                db.query(model.create, [Date().split(' ').slice(0, 4).join(' '), hour, data, equipment_id, feeder_name, current_id, station, level, feeder_name, type]);
             }
         })
         .then(respo => res.send({res: respo}))
