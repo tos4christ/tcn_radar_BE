c7e6b92dfc8172a70fd0f518adbc6eb12a1b212b
tosin fetuga <gnztrades@gmail.com>
2022-02-17 00:16:18 +0100

    fix bug

--- models/station_line_load.js
+++ models/station_line_load.js
@@ -5,8 +5,8 @@
 // Step 3: while looping the lines store their MW and Hour for each key and object in  json
 const sll = {
     get_stations: 'SELECT id, name from station_table',
-    get_lines: '',
-    get_line_load: ''
+    get_lines: 'SELECT * FROM equipment_table where station_id=$1 AND level=$2 AND type=$3',
+    get_line_load: 'SELECT mw, hour FROM power_table WHERE equipment_name=$1 AND level=330 AND type=line ORDER BY hour'
 }
 
 exports.module = sll;
