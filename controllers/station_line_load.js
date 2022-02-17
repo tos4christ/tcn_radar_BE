c2d77092c882ea09ea12035bcfadda927356c6a9
oluwatosin <gnztrades@gmail.com>
2022-02-17 18:50:41 +0100

    lines logic update

--- controllers/station_line_load.js
+++ controllers/station_line_load.js
@@ -5,35 +5,52 @@ const sll = {};
 // Controller first checks to see if the particular row exists or not, this determines
 // If the row will be created or displayed.
 sll.get = (req, res, next) => {
-    // use current_id, equipment_name and level to recognize a current item
-    const { query } = req;
-    const { current_id, date, level, type } = query;
-    // Add date query to this in order to select other days
-    db.query(model.get, [current_id, date, Number(level), type])
-        .then(resp => {
-            res.send({res: resp.rows[0] })
-        }).catch(err => console);        
+    const station_data = {};
+    db.query(model.get_stations)
+    .then(response => {
+        // this returns the stations object with id and name
+        const stations = response.rows;
+        stations.forEach(station => {
+            // Get the lines for each station and store the key pair of the line and arrays
+            // of the 24hours power in the value of the key for the equipment
+            station_data[station.name] = {};
+            // this returns all the  lines belonging  to a station
+            db.query(model.get_lines, [station.id, 330, 'line'])
+                .then(equipment_response => {
+                    const equipments = equipment_response.rows;
+                    equipments.forEach(equipment => {
+                        
+                    })
+                })
+        })
+        // return;
+        // then on each station run the query to get the equipment belonging to each stations
+        // res.send({res: resp.rows[0] })
+    }).catch(err => console);   
+    
+    // Get all the staions for whose data we need to collate
+         
 }
 
-sll.post = (req, res, next) => {
-    // In order to recognize other category of current, add equipment_name, level and type to what to extract from query and add to database
-    const { data } = req.body;
-    // const { data } = bodyData;
-    const { query } = req;
-    const { current_id,  station, feeder_name, type, level, date } = query;
-    // check if the data exists then switch between posting and updating    
-    db.query(model.get, [current_id, date, level, type])
-        .then(resp => {            
-            if(resp.rowCount > 0) {
-                db.query(model.update, [data, current_id]);
-            } else {
-                const hour = current_id.split('-').pop();
-                const equipment_id = 3;                
-                db.query(model.create, [date, hour, data, equipment_id, feeder_name, current_id, station, Number(level), feeder_name, type]);
-            }
-        })
-        .then(respo => res.send({res: respo}))
-        .catch(err => console.log(err))
-}
+// sll.post = (req, res, next) => {
+//     // In order to recognize other category of current, add equipment_name, level and type to what to extract from query and add to database
+//     const { data } = req.body;
+//     // const { data } = bodyData;
+//     const { query } = req;
+//     const { current_id,  station, feeder_name, type, level, date } = query;
+//     // check if the data exists then switch between posting and updating    
+//     db.query(model.get, [current_id, date, level, type])
+//         .then(resp => {            
+//             if(resp.rowCount > 0) {
+//                 db.query(model.update, [data, current_id]);
+//             } else {
+//                 const hour = current_id.split('-').pop();
+//                 const equipment_id = 3;                
+//                 db.query(model.create, [date, hour, data, equipment_id, feeder_name, current_id, station, Number(level), feeder_name, type]);
+//             }
+//         })
+//         .then(respo => res.send({res: respo}))
+//         .catch(err => console.log(err))
+// }
 
 module.exports =  sll;
--- models/station_line_load.js
+++ models/station_line_load.js
@@ -3,10 +3,10 @@
 // and level=330 and type=line, store all these in an object with the station as the key,each line as a subkey and an array
 // of their values as the values of the subkeys
 // Step 3: while looping the lines store their MW and Hour for each key and object in  json
-const sll = {
-    get_stations: 'SELECT id, name from station_table',
+var sll = {
+    get_stations: 'SELECT * FROM station_table',
     get_lines: 'SELECT * FROM equipment_table where station_id=$1 AND level=$2 AND type=$3',
     get_line_load: 'SELECT mw, hour FROM power_table WHERE equipment_name=$1 AND level=330 AND type=line ORDER BY hour'
 }
 
-exports.module = sll;
+module.exports = sll;
