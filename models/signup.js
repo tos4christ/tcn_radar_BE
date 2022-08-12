611241c2724e621c208a9687d59b1ef40c1c00ca
tos4christ <gnztrades@gmail.com>
2022-08-12 18:40:20 +0100

    update for signup and signin

--- models/signup.js
+++ models/signup.js
@@ -1,6 +1,6 @@
 const signup = {
-    create: 'INSERT INTO users(name, email, password, role) VALUES($1, $2, $3, $4) RETURNING *',
-    get: 'SELECT * FROM users_table where email=$1'
+    create: 'INSERT INTO users(name, email, password, role, created_at) VALUES($1, $2, $3, $4, $5) RETURNING *',
+    get: 'SELECT * FROM users where email=$1'
 }
 
 module.exports =  signup;
