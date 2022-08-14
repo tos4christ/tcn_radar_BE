e9e8a6481b9cd6772199e631847a113bee2c20df
tos4christ <gnztrades@gmail.com>
2022-08-14 09:34:41 +0100

    removed jwt authentication from line routes temporarily for test

--- utility/jwtCheck.js
+++ utility/jwtCheck.js
@@ -3,10 +3,11 @@ var jwt = require('jsonwebtoken');
 const jwtCheck = (req, res, next) => {
   console.log(req.headers, 'the request token')
   if (!req.headers.authorization) {
+    return res.redirect('http://tcnnas.org');
     return res.status(401).json({
       status: 'error',
       error: 'Unauthorized access, you need to be logged in as a registered user'
-    }).redirect('http://tcnnas.org');
+    })
   }
   if (req.headers.authorization) {
     const requestToken = req.headers.authorization.split(' ')[1] ? req.headers.authorization.split(' ')[1] : req.headers.authorization;
