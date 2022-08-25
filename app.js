3691914661f8f3805e000cef6bb70f05a371c992
tos4christ <gnztrades@gmail.com>
2022-08-25 16:20:37 +0100

    return the signup route from the express aj to stop react control the signup get page

--- app.js
+++ app.js
@@ -78,6 +78,7 @@ app.use('/reports', reportsRouter);
 app.use('/equipment', equipmentRouter);
 app.use('/tap', tapRouter);
 app.use('/signin', signinRouter);
+app.use('/signup', signupRouter);
 app.use('/mx', mxRouter);
 app.use('/sll', sllRouter);
 app.use('/lines', linesRouter);
