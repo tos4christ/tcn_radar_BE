934a737c543698fb7a9aed61a6142e37e1548812
tos4christ <gnztrades@gmail.com>
2022-09-29 15:55:30 +0100

    incorporate new feature to add total power stations byt the seconds

--- utility/powerAdder.js
+++ utility/powerAdder.js
@@ -459,14 +459,14 @@ function addSimilarEquipment(array) {
     const finalArray = [];
     // get the key for the first item
     const key = Object.keys(array[0]);
-    console.log(key)
-    finalArray.push(...array[0][key]);
+    console.log(key[0])
+    finalArray.push(...array[0][key[0]]);
     if (array.length < 2) {
         return finalArray;
     }
     for (let i=1; i < array.length; i++) {
         const key = Object.keys(array[i]);
-        const current_array = array[i][key];
+        const current_array = array[i][key[0]];
         current_array.forEach( (item, index) => {
             finalArray[index].mw += finalArray[index].mw + item.mw;
         });
