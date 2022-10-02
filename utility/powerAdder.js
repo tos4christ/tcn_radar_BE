93d46a0eaeace9c744b2a87c58aac6d80765e337
tos4christ <gnztrades@gmail.com>
2022-10-02 10:23:32 +0100

    fix bug

--- utility/powerAdder.js
+++ utility/powerAdder.js
@@ -379,8 +379,8 @@ function addSimilarEquipment(array, station_name) {
         finalArray.push(...array[0][key[0]][0]);
 
         if (station_name === 'OLORUNSOGO (GAS)') {
-            console.log(JSON.stringify(array[0][key[0]][0]), key[0], ' the olorunsogo gas');
-            console.log(JSON.stringify(array[2][key[2]][0]), key[2], ' the olorunsogo gas');
+            console.log(JSON.stringify(array[0][key[0]]), key[0], ' the olorunsogo gas');
+            console.log(JSON.stringify(array[2][key[2]]), key[2], ' the olorunsogo gas');
         }
         // This checks if there is only one object in the add
         // In this case just return the only array available
