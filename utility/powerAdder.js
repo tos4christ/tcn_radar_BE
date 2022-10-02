43652b41f240d42f3a6b063082d82628f173d0b3
tos4christ <gnztrades@gmail.com>
2022-10-02 15:47:45 +0100

    fixing bug

--- utility/powerAdder.js
+++ utility/powerAdder.js
@@ -383,7 +383,7 @@ function addSimilarEquipment(array_in, station_name) {
     // check to see it is an array of array
     if(array[0][key[0]].length > 0) {
         const newArr = [...array[0][key[0]][0]]
-        finalArray.push(newArr.concat());
+        finalArray.push(...newArr.concat());
         // This checks if there is only one object in the add
         // In this case just return the only array available
         // or an empty array if nothing exists
