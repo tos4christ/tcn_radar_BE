2160bbe3b8dd9b0ec2551afa9de3aae59115927d
tos4christ <gnztrades@gmail.com>
2022-10-16 09:30:01 +0100

    remove console log

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool();
 
 pool.on('connect', () => {
-    console.log('local db connected')
+    // console.log('local db connected')
 })
 
 module.exports = pool;
