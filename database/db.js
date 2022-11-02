545780acbd9683af71e0c7cce829611ecd274684
tos4christ <gnztrades@gmail.com>
2022-11-02 11:46:43 +0100

    use a new client to download data for tem

--- database/db.js
+++ database/db.js
@@ -1,6 +1,12 @@
 const { Pool } = require('pg');
 
-const pool = new Pool();
+const pool = new Pool({
+    password: '000000',
+    user: 'postgres',
+    database: 'tcn_nas',
+    port: 5432,
+    host: 'localhost'
+});
 
 pool.on('connect', () => {
     // console.log('local db connected')
