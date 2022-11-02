671ef18f2ec9d1d3b969982a7a19d2c39fb4a003
tos4christ <gnztrades@gmail.com>
2022-11-02 12:24:12 +0100

    use a new client to download data for tem

--- models/lines.js
+++ models/lines.js
@@ -17,7 +17,7 @@ const lines = {
         'jebbaTs', 'okpaiGs', 'deltaGs', 'kainjiTs', 'egbinPs', 'afamIv_vPs', 'shiroroPs', 'odukpaniNippPs',
         'transamadiGs', 'afamVPs'
     ) 
-    group by station, line_name, id order by station, line_name, time`,
+    group by station, line_name, id, date, mw, amp, kv, level, equipment_id, mvar, variant, time order by station, line_name, time`,
     get_downtime: 'select * from lines_table where mw=0 and kv=0 and time between $1 and $2 and line_name=$3 and station=$4 order by time',    
     get_profile_max: (parameter, station, line, start, end) => {
         return `select hour, minute, seconds, ${parameter}, date from lines_table where station='${station}' and line_name='${line}' and time between ${start} and ${end} and 
