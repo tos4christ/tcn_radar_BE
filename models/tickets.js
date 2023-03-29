d515bf1408abef44bc40ecea91a53c4be25aaa0d
tos4christ <gnztrades@gmail.com>
2023-03-29 07:30:37 +0100

    change the correct id using the ticket id

--- models/tickets.js
+++ models/tickets.js
@@ -3,7 +3,7 @@ const tickets = {
     update_ticket: 'UPDATE tickets_table SET station=$1, equipment=$2, comment=$3, priority=$4 where disco=$5 and ticket_id=$6',
     update_appr_1: 'UPDATE tickets_table SET appr_one=$1, status=$2 where disco=$3 and ticket_id=$4',
     update_appr_2: 'UPDATE tickets_table SET appr_two=$1, status=$2 where disco=$3 and ticket_id=$4',
-    update_appr_3: 'UPDATE tickets_table SET appr_three=$1, status=$2 where disco=$3 and ticket_id=$4',
+    update_appr_3: 'UPDATE tickets_table SET appr_three=$1, status=$2 where disco=$3 and id=$4',
     get_all_tickets_disco: 'select * from tickets_table where disco=$1',
     get_all_tickets_tcn: 'select * from tickets_table',
     get_pending_tickets: 'select * from tickets_table where disco=$1 and status=$2',   
