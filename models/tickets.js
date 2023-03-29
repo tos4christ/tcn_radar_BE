73ea8787c5fc4c4ff357530b38a541c7a873d558
tos4christ <gnztrades@gmail.com>
2023-03-29 07:32:48 +0100

    change the correct id using the ticket id

--- models/tickets.js
+++ models/tickets.js
@@ -1,8 +1,8 @@
 const tickets = {
     create_ticket: 'INSERT INTO tickets_table(disco, station, equipment, comment, date, ticket_id, status, priority) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *',
-    update_ticket: 'UPDATE tickets_table SET station=$1, equipment=$2, comment=$3, priority=$4 where disco=$5 and ticket_id=$6',
-    update_appr_1: 'UPDATE tickets_table SET appr_one=$1, status=$2 where disco=$3 and ticket_id=$4',
-    update_appr_2: 'UPDATE tickets_table SET appr_two=$1, status=$2 where disco=$3 and ticket_id=$4',
+    update_ticket: 'UPDATE tickets_table SET station=$1, equipment=$2, comment=$3, priority=$4 where disco=$5 and id=$6',
+    update_appr_1: 'UPDATE tickets_table SET appr_one=$1, status=$2 where disco=$3 and id=$4',
+    update_appr_2: 'UPDATE tickets_table SET appr_two=$1, status=$2 where disco=$3 and id=$4',
     update_appr_3: 'UPDATE tickets_table SET appr_three=$1, status=$2 where disco=$3 and id=$4',
     get_all_tickets_disco: 'select * from tickets_table where disco=$1',
     get_all_tickets_tcn: 'select * from tickets_table',
