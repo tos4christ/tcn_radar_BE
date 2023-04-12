dd4af055a9accc0aab98803aaf62390d4bb21331
tos4christ <gnztrades@gmail.com>
2023-04-12 12:12:23 +0100

    save weather data to the database

--- models/weather.js
+++ models/weather.js
@@ -1,8 +1,13 @@
 const lines = {
-    create: 'INSERT INTO weather_table(date, hour, minute, seconds, kv, mw, mvar, amp, equipment_id, station, level, line_name, variant, time) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *',
+    create: `INSERT INTO 
+                weather_table(date, time, station_name, lon, lat, main_weather, weather_id, 
+                    main_weather_description, weather_icon, main_temperature, ambient_temperature,
+                    main_pressure, main_humidity, wind_speed, wind_degree, wind_gust) 
+                VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16) RETURNING *`,
     get_station_coords: `SELECT * from station_coords_2`,
     
 }
+
 
 // Todo list
 // add one to the month and check if needed in the hour in order to make 
