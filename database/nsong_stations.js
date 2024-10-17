ca6119526f046d0edeffe59975b7b971aea964c5
tos4christ <gnztrades@gmail.com>
2024-10-17 09:45:15 +0100

    change geregu nipp to zero on nsong

--- database/nsong_stations.js
+++ database/nsong_stations.js
@@ -761,9 +761,10 @@ module.exports = (data) => {
                         return acc
                     }
                 },0)
-                res_data['GEREGU NIPP (GAS)'].mw = mw_sum;
-                res_data['GEREGU NIPP (GAS)'].amp = amp_sum;
-                res_data['GEREGU NIPP (GAS)'].mvar = mvar_sum;
+                // turn MW, MVAR and AMPs to zero pending change
+                res_data['GEREGU NIPP (GAS)'].mw = mw_sum * 0;
+                res_data['GEREGU NIPP (GAS)'].amp = amp_sum * 0;
+                res_data['GEREGU NIPP (GAS)'].mvar = mvar_sum * 0;
                 res_data['GEREGU NIPP (GAS)'].time = time;
                 res_data['GEREGU NIPP (GAS)'].seconds = seconds;
                 res_data['GEREGU NIPP (GAS)'].kv = max_voltage;
