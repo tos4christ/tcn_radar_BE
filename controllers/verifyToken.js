7d31a8bb5c28bf19152b72d9d21bd3432afb4180
tos4christ <gnztrades@gmail.com>
2025-03-29 13:34:14 +0100

    add a verify token route to verify logged in users

---
+++ controllers/verifyToken.js
@@ -0,0 +1,37 @@
+var model = require('../models/equipment');
+var db = require('../database/db');
+var jwt = require('jsonwebtoken');
+
+const decodeToken = (token) => {
+    try {
+      // Verify and Decode the token
+      // jwt.decode(token); 
+      const decoded = jwt.verify(token, process.env.TOKENKEY);    
+      return decoded;
+    } catch (error) {
+      console.error("Error decoding token:", error);
+      return null;
+    }
+  };
+
+const verifyToken = {};
+// Controller first checks to see if the particular row exists or not, this determines
+
+
+verifyToken.post = (req, res, next) => {
+    const { token } = req.body;
+    const decodedToken = decodeToken(token);
+    console.log(decodedToken, 'the decoded token');
+    if (decodedToken) {
+      res.status(200).send({
+        status: 'Success',
+        data: {
+          message: 'Token is valid',
+          decodedToken,
+          isLoggedIn: true
+        }
+      });
+    }
+}
+
+module.exports =  verifyToken;
