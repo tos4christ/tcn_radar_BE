141aaaaab817ace8b75ec7c3b9f5873efa230aec
tos4christ <gnztrades@gmail.com>
2025-03-29 11:35:06 +0100

    fix bug

--- controllers/signin.js
+++ controllers/signin.js
@@ -52,9 +52,9 @@ signin.post = (req, res) => {
         }
       };
       // console.log(responseBody, 'the password match');
-      res.redirect('/home');
+      // res.redirect('/home');
       // res.status(200).send(responseBody);
-      //res.status(200).send(responseBody); 
+      res.status(200).send(responseBody); 
       next();           
     }
   })
