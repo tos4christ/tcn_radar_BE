72c418475158352c5d0811896323ff6b2a90646f
tos4christ <gnztrades@gmail.com>
2025-03-29 11:06:21 +0100

    fix bug

--- app.js
+++ app.js
@@ -123,10 +123,10 @@ app.use('/api/weather', weatherRouter);
 // })
 
 // Function to intercept disabled routes and not process them sending 404 error
-app.get('/home', (req, res) => {
-  res.status(200);
-  res.redirect('/home');
-});
+// app.get('/home', (req, res) => {
+//   res.status(200);
+//   res.redirect('/home');
+// });
 app.get('/nccnasfullpage', (req, res) => {
   res.status(404);
   res.end('NOT FOUND');
