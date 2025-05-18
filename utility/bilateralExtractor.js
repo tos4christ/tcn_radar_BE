e86a3d3b5a8429eb92b33fe33338274546ff15f9
tos4christ <gnztrades@gmail.com>
2025-05-18 10:29:09 +0100

    fix bug: add system plannning functions to download bilateral data

--- utility/bilateralExtractor.js
+++ utility/bilateralExtractor.js
@@ -127,7 +127,7 @@ module.exports = ( data ) => {
         {
             "ikejaWest-sakate": [
                 {
-                    "ikejaWest-sakate": []
+                    "l1": []
                 }
             ]
         }
@@ -1075,10 +1075,10 @@ function Station_Adder(station_array) {
             if (station_name === 'Ikeja West-Sakete 330kV Line1') {
                 const temp_hold = [];
                 const station_to_add = station_array.filter( sa => Object.keys(sa)[0] === 'ikejaWest-sakate');
-                console.log(JSON.stringify(station_to_add), '  the station to add ikeja west');
+                //console.log(JSON.stringify(station_to_add), '  the station to add ikeja west');
                 // Get the list of equipment objects from the stations
                 // remember to filter equipment in the cases where not all is required
-                const equipment_to_sum = station_to_add[0]['ikejaWest-sakate'];
+                const equipment_to_sum = station_to_add[0]['l1'];
                 // run logic only if there is an equipment to iterate
                 if (equipment_to_sum?.length > 0) {
                     equipment_to_sum.forEach((equip, index) => {
