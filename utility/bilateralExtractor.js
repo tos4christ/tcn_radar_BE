ca61ff7340e1be4c48c974e83081965294a94ac7
tos4christ <gnztrades@gmail.com>
2025-05-18 14:52:34 +0100

    fix bilateral download complete

--- utility/bilateralExtractor.js
+++ utility/bilateralExtractor.js
@@ -297,7 +297,7 @@ function Station_Adder(station_array) {
             if (station_name === 'PHEONIX STEEL IKORODU') {
                 const temp_hold = [];
                 const station_to_add = station_array.filter( sa => Object.keys(sa)[0] === 'phoenix');
-                console.log(JSON.stringify(station_to_add), ' the station to add phoenix');
+                // console.log(JSON.stringify(station_to_add), ' the station to add phoenix');
                 // Get the list of equipment objects from the stations
                 // remember to filter equipment in the cases where not all is required
                 const equipment_to_sum = station_to_add[0]['phoenix'].filter( sa => Object.keys(sa)[0] === 'pt1');
@@ -348,7 +348,7 @@ function Station_Adder(station_array) {
             if (station_name === 'PULKIT ALLOY & STEEL IKORODU') {
                 const temp_hold = [];
                 const station_to_add = station_array.filter( sa => Object.keys(sa)[0] === 'pulkitSteel');
-                console.log(JSON.stringify(station_to_add), ' the station to add pulkitSteel');
+                // console.log(JSON.stringify(station_to_add), ' the station to add pulkitSteel');
                 // Get the list of equipment objects from the stations
                 // remember to filter equipment in the cases where not all is required
                 const equipment_to_sum = station_to_add[0]['pulkitSteel'].filter( sa => Object.keys(sa)[0] === 'psl1');                
@@ -397,7 +397,7 @@ function Station_Adder(station_array) {
             if (station_name === 'SUNFLAG IRON & STEEL IKORODU') {
                 const temp_hold = [];
                 const station_to_add = station_array.filter( sa => Object.keys(sa)[0] === 'sunflag');
-                console.log(JSON.stringify(station_to_add), ' the station to add sunflag');
+                // console.log(JSON.stringify(station_to_add), ' the station to add sunflag');
                 // Get the list of equipment objects from the stations
                 // remember to filter equipment in the cases where not all is required
                 const equipment_to_sum = station_to_add[0]['sunflag'].filter( sa => Object.keys(sa)[0] === 'sl1');                
