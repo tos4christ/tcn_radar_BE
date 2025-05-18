bb0cbe3c1035643860657a2cf86a3ade70d39400
tos4christ <gnztrades@gmail.com>
2025-05-18 12:28:11 +0100

    inspect data

--- utility/bilateralExtractor.js
+++ utility/bilateralExtractor.js
@@ -1081,8 +1081,8 @@ function Station_Adder(station_array) {
                 const station_to_add = station_array.filter( sa => Object.keys(sa)[0] === 'Er-Kang');
                 // Get the list of equipment objects from the stations
                 // remember to filter equipment in the cases where not all is required
-                const equipment_to_sum = station_to_add[0]['Er-Kang'][0]['Er-Kang'];
-                console.log(JSON.stringify(equipment_to_sum), '  the equipment to sum for er-kang');
+                const equipment_to_sum = station_to_add[0]['Er-Kang'];
+                //console.log(JSON.stringify(equipment_to_sum), '  the equipment to sum for er-kang');
                 // run logic only if there is an equipment to iterate
                 if (equipment_to_sum?.length > 0) {
                     equipment_to_sum.forEach((equip, index) => {
