f20a9a55af0980a37a6124b0a14f0edd75673376
tos4christ <gnztrades@gmail.com>
2025-06-07 22:38:23 +0100

    fix bug: socket Io instance sharing

---
+++ database/sockets.js
@@ -0,0 +1,17 @@
+const { Server } = require('socket.io');
+
+let io;
+
+module.exports = {
+    init: (server) => {
+        io = new Server(server);
+        io.on('connection', (socket) => {
+            console.log('Client connected to global Socket.IO instance');
+        });
+        return io;
+    },
+    getIO: () => {
+        if (!io) throw new Error("Socket.IO has not been initialized!");
+        return io;
+    }
+};
